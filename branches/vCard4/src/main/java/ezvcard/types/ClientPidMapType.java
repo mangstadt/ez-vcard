cc63327eb2bc0450cb76777d1c5b1fd2457c0bb9
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2012-07-29 17:04:05 +0000

    Added more types.

---
+++ branches/vCard4/src/main/java/ezvcard/types/ClientPidMapType.java
@@ -0,0 +1,101 @@
+package ezvcard.types;
+
+import java.util.List;
+
+import ezvcard.VCardVersion;
+import ezvcard.io.CompatibilityMode;
+import ezvcard.util.VCardStringUtils;
+
+/*
+ Copyright (c) 2012, Michael Angstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ */
+
+/**
+ * Represents the CLINTPIDMAP type.
+ * @author Michael Angstadt
+ */
+public class ClientPidMapType extends VCardType {
+	public static final String NAME = "CLIENTPIDMAP";
+
+	private int pid;
+	private String uri;
+
+	/**
+	 * @param name the type name
+	 */
+	public ClientPidMapType() {
+		super(NAME);
+	}
+
+	/**
+	 * @param name the type name
+	 * @param value the value
+	 */
+	public ClientPidMapType(int pid, String uri) {
+		super(NAME);
+		this.pid = pid;
+		this.uri = uri;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	@Override
+	public VCardVersion[] getSupportedVersions() {
+		return new VCardVersion[] { VCardVersion.V4_0 };
+	}
+
+	@Override
+	protected String doMarshalValue(VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		return pid + ";" + VCardStringUtils.escapeText(uri);
+	}
+
+	@Override
+	protected void doUnmarshalValue(String value, VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		String split[] = value.split(";", 2);
+		if (split.length < 2) {
+			warnings.add("Incorrect format of " + NAME + " type value: \"" + value + "\"");
+		} else {
+			pid = Integer.parseInt(split[0]);
+			uri = VCardStringUtils.unescape(split[1]);
+		}
+	}
+}
