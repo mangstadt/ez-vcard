bb69ef08b849ea8e087d8ddc46afa5f9c0a61a47
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2012-08-04 14:04:35 +0000

    Added support for text values to the KEY property.

--- branches/vCard4/src/main/java/ezvcard/types/KeyType.java
+++ branches/vCard4/src/main/java/ezvcard/types/KeyType.java
@@ -2,9 +2,13 @@ package ezvcard.types;
 
 import java.util.List;
 
+import ezvcard.VCard;
+import ezvcard.VCardSubTypes;
 import ezvcard.VCardVersion;
 import ezvcard.io.CompatibilityMode;
 import ezvcard.parameters.KeyTypeParameter;
+import ezvcard.parameters.ValueParameter;
+import ezvcard.util.VCardStringUtils;
 
 /*
  Copyright (c) 2012, Michael Angstadt
@@ -42,12 +46,54 @@ import ezvcard.parameters.KeyTypeParameter;
 public class KeyType extends BinaryType<KeyTypeParameter> {
 	public static final String NAME = "KEY";
 
+	private String text;
+
 	public KeyType() {
 		super(NAME);
 	}
 
+	/**
+	 * @param data the binary data
+	 * @param type the type of key
+	 */
 	public KeyType(byte data[], KeyTypeParameter type) {
 		super(NAME, data, type);
+	}
+
+	/**
+	 * @param url the URL to the key (vCard 4.0 only)
+	 * @param type the type of key
+	 */
+	public KeyType(String url, KeyTypeParameter type) {
+		super(NAME, url, type);
+	}
+
+	/**
+	 * Sets a plain text representation of the key.
+	 * @param key the key in plain text
+	 * @param type the key type
+	 */
+	public void setText(String text, KeyTypeParameter type) {
+		setText(text);
+		setType(type);
+	}
+
+	/**
+	 * Sets a plain text representation of the key.
+	 * @param key the key in plain text
+	 */
+	private void setText(String text) {
+		this.text = text;
+		data = null;
+		url = null;
+	}
+
+	/**
+	 * Gets the plain text representation of the key.
+	 * @return the key in plain text
+	 */
+	public String getText() {
+		return text;
 	}
 
 	@Override
@@ -76,10 +122,36 @@ public class KeyType extends BinaryType<KeyTypeParameter> {
 	}
 
 	@Override
+	protected VCardSubTypes doMarshalSubTypes(VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode, VCard vcard) {
+		if (text != null) {
+			VCardSubTypes copy = new VCardSubTypes(subTypes);
+			copy.setValue(ValueParameter.TEXT);
+			copy.setEncoding(null);
+			if (version == VCardVersion.V4_0) {
+				copy.setType(null);
+			} else {
+				copy.setMediaType(null);
+			}
+			return copy;
+		}
+		return super.doMarshalSubTypes(version, warnings, compatibilityMode, vcard);
+	}
+
+	@Override
 	protected String doMarshalValue(VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		if (text != null) {
+			return VCardStringUtils.escapeText(text);
+		}
+
 		if ((version == VCardVersion.V2_1 || version == VCardVersion.V3_0) && getUrl() != null) {
 			warnings.add("vCard version " + version + " does not allow URLs to be used in the " + NAME + " type.");
 		}
 		return super.doMarshalValue(version, warnings, compatibilityMode);
+	}
+
+	@Override
+	protected void cannotUnmarshalValue(String value, VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		//unmarshal it as a plain text key
+		setText(VCardStringUtils.unescape(value));
 	}
 }
