18dfa1de7963e8055ca7fb527d47457efcc21cda
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2012-08-11 14:45:26 +0000

    Added tag for 0.2.0.

---
+++ tags/0.2.0/src/main/java/ezvcard/types/DateOrTimeType.java
@@ -0,0 +1,268 @@
+package ezvcard.types;
+
+import java.util.Date;
+import java.util.List;
+
+import ezvcard.VCard;
+import ezvcard.VCardSubTypes;
+import ezvcard.VCardVersion;
+import ezvcard.io.CompatibilityMode;
+import ezvcard.parameters.CalscaleParameter;
+import ezvcard.parameters.ValueParameter;
+import ezvcard.util.ISOFormat;
+import ezvcard.util.VCardDateFormatter;
+import ezvcard.util.VCardStringUtils;
+
+/*
+ Copyright (c) 2012, Michael Angstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ */
+
+/**
+ * Represents a type that contains a date and/or time (for example, the BDAY
+ * type).
+ * @author Michael Angstadt
+ */
+public class DateOrTimeType extends VCardType {
+	private String text;
+	private Date date;
+	private String reducedAccuracyDate;
+
+	/**
+	 * True if the "date" or "reduceAccuracyDate" fields have a time component,
+	 * false if they just contain a date.
+	 */
+	private boolean dateHasTime;
+
+	/**
+	 * @param typeName the name of the type (e.g. "BDAY")
+	 */
+	public DateOrTimeType(String typeName) {
+		super(typeName);
+	}
+
+	/**
+	 * @param typeName the name of the type (e.g. "BDAY")
+	 * @param date the date value
+	 */
+	public DateOrTimeType(String typeName, Date date) {
+		super(typeName);
+		setDate(date, false);
+	}
+
+	/**
+	 * Gets the date value.
+	 * @return the date value or null if not set
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * Sets the value of this type to a complete date.
+	 * @param date the date
+	 * @param dateHasTime true if the date contains a time component, false if
+	 * it's just a date
+	 */
+	public void setDate(Date date, boolean dateHasTime) {
+		this.date = date;
+		this.dateHasTime = dateHasTime;
+		text = null;
+		reducedAccuracyDate = null;
+	}
+
+	/**
+	 * Gets the reduced accuracy date string. This is only supported by vCard
+	 * 4.0.
+	 * @return the reduced accuracy date string or null if not set
+	 * @see "<a href="
+	 * http://tools.ietf.org/html/rfc6350">RFC 6350</a> p.12-14 for examples"
+	 */
+	public String getReducedAccuracyDate() {
+		return reducedAccuracyDate;
+	}
+
+	/**
+	 * Sets the value of this type to a "reduced accuracy" date. This is only
+	 * supported by vCard 4.0.
+	 * @param reducedAccuracyDate the reduced accuracy date (e.g "--0210" for
+	 * "February 10")
+	 * @see "<a href="
+	 * http://tools.ietf.org/html/rfc6350">RFC 6350</a> p.12-14 for examples"
+	 */
+	public void setReducedAccuracyDate(String reducedAccuracyDate) {
+		this.reducedAccuracyDate = reducedAccuracyDate;
+		dateHasTime = reducedAccuracyDate.contains("T");
+		text = null;
+		date = null;
+	}
+
+	/**
+	 * Gets the text value of this type. This is only supported by vCard 4.0.
+	 * @return the text value or null if not set
+	 */
+	public String getText() {
+		return text;
+	}
+
+	/**
+	 * Sets the value of this type to a text string. This is only supported by
+	 * vCard 4.0.
+	 * @param text the text value
+	 */
+	public void setText(String text) {
+		this.text = text;
+		date = null;
+		reducedAccuracyDate = null;
+	}
+
+	/**
+	 * Gets the type of calendar this date uses.
+	 * <p>
+	 * vCard versions: 4.0
+	 * </p>
+	 * @return the type of calendar or null if not set
+	 */
+	public CalscaleParameter getCalscale() {
+		return subTypes.getCalscale();
+	}
+
+	/**
+	 * Sets the type of calendar this date uses.
+	 * <p>
+	 * vCard versions: 4.0
+	 * </p>
+	 * @param calscale the type of calendar or null to remove
+	 */
+	public void setCalsclae(CalscaleParameter calscale) {
+		subTypes.setCalscale(calscale);
+	}
+
+	/**
+	 * Gets the ALTID.
+	 * <p>
+	 * vCard versions: 4.0
+	 * </p>
+	 * @return the ALTID or null if it doesn't exist
+	 * @see VCardSubTypes#getAltId
+	 */
+	public String getAltId() {
+		return subTypes.getAltId();
+	}
+
+	/**
+	 * Sets the ALTID.
+	 * <p>
+	 * vCard versions: 4.0
+	 * </p>
+	 * @param altId the ALTID or null to remove
+	 * @see VCardSubTypes#setAltId
+	 */
+	public void setAltId(String altId) {
+		subTypes.setAltId(altId);
+	}
+
+	@Override
+	protected VCardSubTypes doMarshalSubTypes(VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode, VCard vcard) {
+		VCardSubTypes copy = new VCardSubTypes(subTypes);
+		if (version == VCardVersion.V4_0) {
+			if (date != null || reducedAccuracyDate != null) {
+				copy.setValue(ValueParameter.DATE_AND_OR_TIME);
+				if (getCalscale() == null) {
+					copy.setCalscale(CalscaleParameter.GREGORIAN);
+				}
+			} else if (text != null) {
+				copy.setValue(ValueParameter.TEXT);
+			}
+		} else {
+			if (dateHasTime) {
+				copy.setValue(ValueParameter.DATE_TIME);
+			} else {
+				copy.setValue(ValueParameter.DATE);
+			}
+		}
+		return copy;
+	}
+
+	@Override
+	protected String doMarshalValue(VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		if (version == VCardVersion.V2_1 || version == VCardVersion.V3_0) {
+			if (text != null) {
+				warnings.add("Text values for the " + typeName + " type are not allowed in vCard version " + version + ".  This type will not be added to the vCard.");
+				return null;
+			} else if (reducedAccuracyDate != null) {
+				warnings.add("Reduced accuracy dates for the " + typeName + " type are not allowed in vCard version " + version + ".  This type will not be added to the vCard.");
+				return null;
+			} else if (date != null) {
+				ISOFormat format = dateHasTime ? ISOFormat.TIME_BASIC : ISOFormat.DATE_BASIC;
+				return VCardDateFormatter.format(date, format);
+			} else {
+				return null;
+			}
+		} else {
+			if (text != null) {
+				return VCardStringUtils.escapeText(text);
+			} else if (reducedAccuracyDate != null) {
+				return reducedAccuracyDate;
+			} else if (date != null) {
+				ISOFormat format = dateHasTime ? ISOFormat.TIME_BASIC : ISOFormat.DATE_BASIC;
+				return VCardDateFormatter.format(date, format);
+			} else {
+				return null;
+			}
+		}
+	}
+
+	@Override
+	protected void doUnmarshalValue(String value, VCardVersion version, List<String> warnings, CompatibilityMode compatibilityMode) {
+		if (version == VCardVersion.V4_0) {
+			if (subTypes.getValue() == ValueParameter.TEXT) {
+				text = VCardStringUtils.unescape(value);
+			} else if (value.contains("-")) {
+				reducedAccuracyDate = value;
+			} else {
+				try {
+					date = VCardDateFormatter.parse(value);
+				} catch (IllegalArgumentException e) {
+					//not all reduced accuracy dates have dashes (e.g. "2012")
+					if (value.matches("\\d+")) {
+						reducedAccuracyDate = value;
+					} else {
+						warnings.add("Date string \"" + value + "\" could not be parsed.  Assuming it's a text value.");
+						text = VCardStringUtils.unescape(value);
+					}
+				}
+			}
+		} else {
+			try {
+				date = VCardDateFormatter.parse(value);
+			} catch (IllegalArgumentException e) {
+				warnings.add("Date string \"" + value + "\" for type \"" + typeName + "\" could not be parsed.");
+			}
+		}
+	}
+}
---
+++ tags/0.2.0/src/test/java/ezvcard/types/DateOrTimeTypeTest.java
@@ -0,0 +1,308 @@
+package ezvcard.types;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import ezvcard.VCard;
+import ezvcard.VCardSubTypes;
+import ezvcard.VCardVersion;
+import ezvcard.io.CompatibilityMode;
+import ezvcard.parameters.CalscaleParameter;
+import ezvcard.parameters.ValueParameter;
+
+/*
+ Copyright (c) 2012, Michael Angstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ The views and conclusions contained in the software and documentation are those
+ of the authors and should not be interpreted as representing official policies, 
+ either expressed or implied, of the FreeBSD Project.
+ */
+
+/**
+ * @author Michael Angstadt
+ */
+public class DateOrTimeTypeTest {
+	@Test
+	public void marshalDate() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+
+		Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(Calendar.YEAR, 1980);
+		c.set(Calendar.MONTH, Calendar.JUNE);
+		c.set(Calendar.DAY_OF_MONTH, 5);
+		Date date = c.getTime();
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.setDate(date, false);
+
+		String expected = "19800605";
+
+		//v2.1
+		VCardVersion version = VCardVersion.V2_1;
+		String actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertEquals(expected, actual);
+		VCardSubTypes subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE, subTypes.getValue());
+
+		//v3.0
+		version = VCardVersion.V3_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertEquals(expected, actual);
+		subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE, subTypes.getValue());
+
+		//v4.0
+		version = VCardVersion.V4_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertEquals(expected, actual);
+		subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE_AND_OR_TIME, subTypes.getValue());
+		assertEquals(CalscaleParameter.GREGORIAN, subTypes.getCalscale());
+	}
+
+	@Test
+	public void marshalDateTime() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+
+		Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(Calendar.YEAR, 1980);
+		c.set(Calendar.MONTH, Calendar.JUNE);
+		c.set(Calendar.DAY_OF_MONTH, 5);
+		c.set(Calendar.HOUR_OF_DAY, 13);
+		c.set(Calendar.MINUTE, 10);
+		c.set(Calendar.SECOND, 20);
+
+		Date date = c.getTime();
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.setDate(date, true);
+
+		//uses the local machine's timezone
+		String expected = "19800605T131020[-\\+]\\d{4}";
+
+		//v2.1
+		VCardVersion version = VCardVersion.V2_1;
+		String actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertTrue(actual.matches(expected));
+		VCardSubTypes subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE_TIME, subTypes.getValue());
+
+		//v3.0
+		version = VCardVersion.V3_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertTrue(actual.matches(expected));
+		subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE_TIME, subTypes.getValue());
+
+		//v4.0
+		version = VCardVersion.V4_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertTrue(actual.matches(expected));
+		subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE_AND_OR_TIME, subTypes.getValue());
+		assertEquals(CalscaleParameter.GREGORIAN, subTypes.getCalscale());
+	}
+
+	@Test
+	public void marshalReducedAccuracyDate() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.setReducedAccuracyDate("--0214");
+
+		String expected = "--0214";
+
+		//v2.1
+		VCardVersion version = VCardVersion.V2_1;
+		String actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertNull(actual); //not supported
+
+		//v3.0
+		version = VCardVersion.V3_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertNull(actual); //not supported
+
+		//v4.0
+		version = VCardVersion.V4_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertEquals(actual, expected);
+		VCardSubTypes subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.DATE_AND_OR_TIME, subTypes.getValue());
+		assertEquals(CalscaleParameter.GREGORIAN, subTypes.getCalscale());
+	}
+
+	@Test
+	public void marshalText() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.setText("Sometime around ;1980;");
+
+		String expected = "Sometime around \\;1980\\;";
+
+		//v2.1
+		VCardVersion version = VCardVersion.V2_1;
+		String actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertNull(actual); //not supported
+
+		//v3.0
+		version = VCardVersion.V3_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertNull(actual); //not supported
+
+		//v4.0
+		version = VCardVersion.V4_0;
+		actual = t.marshalValue(version, warnings, compatibilityMode);
+		assertEquals(actual, expected);
+		VCardSubTypes subTypes = t.marshalSubTypes(version, warnings, compatibilityMode, new VCard());
+		assertEquals(ValueParameter.TEXT, subTypes.getValue());
+		assertNull(subTypes.getCalscale());
+	}
+
+	@Test
+	public void unmarshalDate() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+		VCardSubTypes subTypes = new VCardSubTypes();
+
+		Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(Calendar.YEAR, 1980);
+		c.set(Calendar.MONTH, Calendar.JUNE);
+		c.set(Calendar.DAY_OF_MONTH, 5);
+		Date expected = c.getTime();
+
+		//2.1
+		VCardVersion version = VCardVersion.V2_1;
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, "1980-06-05", version, warnings, compatibilityMode);
+		assertEquals(expected, t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//3.0
+		version = VCardVersion.V3_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, "1980-06-05", version, warnings, compatibilityMode);
+		assertEquals(expected, t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//4.0
+		version = VCardVersion.V4_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, "1980-06-05", version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertEquals("1980-06-05", t.getReducedAccuracyDate()); //it thinks it's reduced accuracy because it has dashes
+		assertNull(t.getText());
+
+		version = VCardVersion.V4_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, "19800605", version, warnings, compatibilityMode);
+		assertEquals(expected, t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+	}
+
+	@Test
+	public void unmarshalReducedAccuracyDate() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+		VCardSubTypes subTypes = new VCardSubTypes();
+
+		String value = "--0201";
+		String expected = value;
+
+		//2.1
+		VCardVersion version = VCardVersion.V2_1;
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//3.0
+		version = VCardVersion.V3_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//4.0
+		version = VCardVersion.V4_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertEquals(expected, t.getReducedAccuracyDate());
+		assertNull(t.getText());
+	}
+
+	@Test
+	public void unmarshalText() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		CompatibilityMode compatibilityMode = CompatibilityMode.RFC2426;
+		VCardSubTypes subTypes = new VCardSubTypes();
+		subTypes.setValue(ValueParameter.TEXT);
+
+		String value = "Some \\;text\\;.";
+		String expected = "Some ;text;.";
+
+		//2.1
+		VCardVersion version = VCardVersion.V2_1;
+		DateOrTimeType t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//3.0
+		version = VCardVersion.V3_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertNull(t.getText());
+
+		//4.0
+		version = VCardVersion.V4_0;
+		t = new DateOrTimeType("DATE");
+		t.unmarshalValue(subTypes, value, version, warnings, compatibilityMode);
+		assertNull(t.getDate());
+		assertNull(t.getReducedAccuracyDate());
+		assertEquals(expected, t.getText());
+	}
+}
