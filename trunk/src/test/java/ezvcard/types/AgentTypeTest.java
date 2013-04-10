1dec23ba78ba5d9614e05e2bea0398c404f8941b
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2013-04-10 00:20:05 +0000

    Fixed an issue where AGENT URLs in hCards would not be unmarshalled and improved the AgentType unit tests.

--- trunk/src/test/java/ezvcard/types/AgentTypeTest.java
+++ trunk/src/test/java/ezvcard/types/AgentTypeTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ezvcard.VCard;
@@ -14,10 +15,12 @@ import ezvcard.VCardSubTypes;
 import ezvcard.VCardVersion;
 import ezvcard.io.CompatibilityMode;
 import ezvcard.io.EmbeddedVCardException;
+import ezvcard.io.SkipMeException;
 import ezvcard.parameters.ValueParameter;
+import ezvcard.util.HtmlUtils;
 
 /*
- Copyright (c) 2012, Michael Angstadt
+ Copyright (c) 2013, Michael Angstadt
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
@@ -49,60 +52,209 @@ import ezvcard.parameters.ValueParameter;
  * @author Michael Angstadt
  */
 public class AgentTypeTest {
-	@Test
-	public void doMarshalValue() throws Exception {
-		VCardVersion version = VCardVersion.V3_0;
-		List<String> warnings = new ArrayList<String>();
-		CompatibilityMode compatibilityMode = CompatibilityMode.RFC;
-		AgentType t;
-		String expected, actual;
+	final List<String> warnings = new ArrayList<String>();
+	final CompatibilityMode compatibilityMode = CompatibilityMode.RFC;
 
-		//with URL
-		t = new AgentType("http://mi5.co.uk/007");
-		expected = "http://mi5.co.uk/007";
-		actual = t.marshalText(version, warnings, compatibilityMode);
-		assertEquals(expected, actual);
+	final VCard vcard = new VCard();
+	final String url = "http://mi5.co.uk/007";
+	final AgentType urlType = new AgentType(url);
+	final AgentType vcardType = new AgentType(vcard);
+	final AgentType emptyType = new AgentType();
 
-		//with vCard
-		VCard vcard = new VCard();
-		t = new AgentType(vcard);
-		try {
-			t.marshalText(version, warnings, compatibilityMode);
-			fail();
-		} catch (EmbeddedVCardException e) {
-			//should be thrown
-			assertEquals(vcard, e.getVCard());
-		}
+	final VCardSubTypes subTypes = new VCardSubTypes();
+	AgentType agentType;
+
+	@Before
+	public void before() {
+		agentType = new AgentType();
+		warnings.clear();
+		subTypes.clear();
 	}
 
 	@Test
-	public void doUnmarshalValue() throws Exception {
+	public void marshalSubTypes_url_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		VCardSubTypes subTypes = urlType.marshalSubTypes(version, warnings, compatibilityMode, vcard);
+
+		assertEquals(1, subTypes.size());
+		assertEquals(ValueParameter.URL, subTypes.getValue());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalSubTypes_url_3_0() {
 		VCardVersion version = VCardVersion.V3_0;
-		List<String> warnings = new ArrayList<String>();
-		CompatibilityMode compatibilityMode = CompatibilityMode.RFC;
-		VCardSubTypes subTypes;
-		AgentType t;
+		VCardSubTypes subTypes = urlType.marshalSubTypes(version, warnings, compatibilityMode, vcard);
 
-		//with URL
-		t = new AgentType();
-		subTypes = new VCardSubTypes();
-		subTypes.setValue(ValueParameter.URL);
-		t.unmarshalText(subTypes, "http://mi5.co.uk/007", version, warnings, compatibilityMode);
-		assertEquals("http://mi5.co.uk/007", t.getUrl());
-		assertNull(t.getVCard());
+		assertEquals(1, subTypes.size());
+		assertEquals(ValueParameter.URI, subTypes.getValue());
+		assertEquals(0, warnings.size());
+	}
 
-		//with vCard
-		VCard vcard = new VCard();
-		t = new AgentType();
-		subTypes = new VCardSubTypes();
+	@Test
+	public void marshalSubTypes_vcard_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		VCardSubTypes subTypes = vcardType.marshalSubTypes(version, warnings, compatibilityMode, vcard);
+
+		assertEquals(0, subTypes.size());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalSubTypes_vcard_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		VCardSubTypes subTypes = vcardType.marshalSubTypes(version, warnings, compatibilityMode, vcard);
+
+		assertEquals(0, subTypes.size());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalText_url_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		String actual = urlType.marshalText(version, warnings, compatibilityMode);
+
+		assertEquals(url, actual);
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalText_url_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		String actual = urlType.marshalText(version, warnings, compatibilityMode);
+
+		assertEquals(url, actual);
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalText_vcard_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
 		try {
-			t.unmarshalText(subTypes, "BEGIN:VCARD\\nEND:VCARD", version, warnings, compatibilityMode);
+			vcardType.marshalText(version, warnings, compatibilityMode);
 			fail();
 		} catch (EmbeddedVCardException e) {
-			//should be thrown
+			assertEquals(vcard, e.getVCard());
+		}
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void marshalText_vcard_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		try {
+			vcardType.marshalText(version, warnings, compatibilityMode);
+			fail();
+		} catch (EmbeddedVCardException e) {
+			assertEquals(vcard, e.getVCard());
+		}
+		assertEquals(0, warnings.size());
+	}
+
+	@Test(expected = SkipMeException.class)
+	public void marshalText_no_url_or_vcard_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		emptyType.marshalText(version, warnings, compatibilityMode);
+	}
+
+	@Test(expected = SkipMeException.class)
+	public void marshalText_no_url_or_vcard_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		emptyType.marshalText(version, warnings, compatibilityMode);
+	}
+
+	@Test
+	public void unmarshalText_url_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		subTypes.setValue(ValueParameter.URL);
+		agentType.unmarshalText(subTypes, url, version, warnings, compatibilityMode);
+
+		assertEquals(url, agentType.getUrl());
+		assertNull(agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalText_url_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		subTypes.setValue(ValueParameter.URI);
+		agentType.unmarshalText(subTypes, url, version, warnings, compatibilityMode);
+
+		assertEquals(url, agentType.getUrl());
+		assertNull(agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalText_vcard_2_1() {
+		VCardVersion version = VCardVersion.V2_1;
+		try {
+			agentType.unmarshalText(subTypes, "", version, warnings, compatibilityMode);
+			fail();
+		} catch (EmbeddedVCardException e) {
 			e.injectVCard(vcard);
 		}
-		assertNull(t.getUrl());
-		assertEquals(vcard, t.getVCard());
+		assertNull(agentType.getUrl());
+		assertEquals(vcard, agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalText_vcard_3_0() {
+		VCardVersion version = VCardVersion.V3_0;
+		try {
+			agentType.unmarshalText(subTypes, "BEGIN:VCARD\\nEND:VCARD", version, warnings, compatibilityMode);
+			fail();
+		} catch (EmbeddedVCardException e) {
+			e.injectVCard(vcard);
+		}
+		assertNull(agentType.getUrl());
+		assertEquals(vcard, agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalHtml_url_link() {
+		org.jsoup.nodes.Element element = HtmlUtils.toElement("<a href=\"" + url + "\"></a>");
+		agentType.unmarshalHtml(element, warnings);
+
+		assertEquals(url, agentType.getUrl());
+		assertNull(agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalHtml_url_text() {
+		org.jsoup.nodes.Element element = HtmlUtils.toElement("<div>" + url + "</div>");
+		agentType.unmarshalHtml(element, warnings);
+
+		assertEquals(url, agentType.getUrl());
+		assertNull(agentType.getVCard());
+		assertEquals(0, warnings.size());
+	}
+
+	@Test
+	public void unmarshalHtml_vcard() {
+		//@formatter:off
+		org.jsoup.nodes.Element element = HtmlUtils.toElement(
+		"<div class=\"agent vcard\">" +
+			"<span class=\"fn\">Jane Doe</span>" +
+			"<div class=\"agent vcard\">" +
+				"<span class=\"fn\">Joseph Doe</span>" +
+			"</div>" +
+		"</div>"
+		);
+		//@formatter:on
+
+		try {
+			agentType.unmarshalHtml(element, warnings);
+			fail();
+		} catch (EmbeddedVCardException e) {
+			e.injectVCard(vcard);
+		}
+
+		assertNull(agentType.getUrl());
+		assertEquals(vcard, agentType.getVCard());
+		assertEquals(0, warnings.size());
 	}
 }
