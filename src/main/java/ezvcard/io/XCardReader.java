1032f20f5d1d2d1e2287c63523fa87d1664aefc1
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2013-05-16 15:24:25 +0000

    Replaced "EZ-vCard" with "ez-vcard".

--- src/main/java/ezvcard/io/XCardReader.java
+++ src/main/java/ezvcard/io/XCardReader.java
@@ -305,8 +305,8 @@ public class XCardReader implements IParser {
 			try {
 				return clazz.newInstance();
 			} catch (Exception e) {
-				//it is the responsibility of the EZ-vCard developer to ensure that this exception is never thrown
-				//all type classes defined in the EZ-vCard library MUST have public, no-arg constructors
+				//it is the responsibility of the ez-vcard developer to ensure that this exception is never thrown
+				//all type classes defined in the ez-vcard library MUST have public, no-arg constructors
 				throw new RuntimeException(e);
 			}
 		} else {
