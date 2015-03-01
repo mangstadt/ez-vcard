fad36f1f42ba30ce1ae06288d483c7d9e552ac1c
mike.angstadt@gmail.com <mike.angstadt@gmail.com@0d0b4aa8-800d-9314-201a-87d9e7845c6b>
2015-03-01 22:31:23 +0000

    Added javadocs.

--- src/main/java/ezvcard/VCardDataType.java
+++ src/main/java/ezvcard/VCardDataType.java
@@ -55,83 +55,139 @@ public class VCardDataType {
 	};
 
 	/**
+	 * <p>
+	 * A uniform resource locator (e.g. "http://www.example.com/image.jpg").
+	 * This data type is only used in 2.1 vCards. All other vCard versions use
+	 * {@link #URI}.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 2.1 (p.18-9)}
 	 */
 	public static final VCardDataType URL = new VCardDataType("url", VCardVersion.V2_1);
 
 	/**
+	 * <p>
+	 * Refers to a MIME entity within an email.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 2.1 (p.8-9)}
 	 */
 	public static final VCardDataType CONTENT_ID = new VCardDataType("content-id", VCardVersion.V2_1);
 
 	/**
+	 * <p>
+	 * A non-textual value, such as a picture or sound file.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 3.0}
 	 */
 	public static final VCardDataType BINARY = new VCardDataType("binary", VCardVersion.V3_0);
 
 	/**
+	 * <p>
+	 * A uniform resource identifier (e.g. "http://www.example.com/image.jpg").
+	 * 2.1 vCards use {@link #URL} instead.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 3.0, 4.0}
 	 */
 	public static final VCardDataType URI = new VCardDataType("uri", VCardVersion.V3_0, VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A plain text value.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 2.1, 3.0, 4.0}
 	 */
 	public static final VCardDataType TEXT = new VCardDataType("text");
 
 	/**
+	 * <p>
+	 * A date that does not have a time component (e.g. "2015-02-16").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 3.0, 4.0}
 	 */
 	public static final VCardDataType DATE = new VCardDataType("date", VCardVersion.V3_0, VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A time that does not have a date component (e.g. "08:34:00").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 3.0, 4.0}
 	 */
 	public static final VCardDataType TIME = new VCardDataType("time", VCardVersion.V3_0, VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A date with a time component (e.g. "2015-02-16 08:34:00").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 3.0, 4.0}
 	 */
 	public static final VCardDataType DATE_TIME = new VCardDataType("date-time", VCardVersion.V3_0, VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * Any sort of date/time combination. The value can be a date (e.g.
+	 * "2015-02-16"), a time (e.g. "08:34:00"), or a date with a time component
+	 * (e.g. "2015-02-16 08:34:00").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType DATE_AND_OR_TIME = new VCardDataType("date-and-or-time", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A specific moment in time. Timestamps should be in UTC time.
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType TIMESTAMP = new VCardDataType("timestamp", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A boolean value ("true" or "false").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType BOOLEAN = new VCardDataType("boolean", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * An integer value (e.g. "42").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType INTEGER = new VCardDataType("integer", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A floating-point value (e.g. "3.14").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType FLOAT = new VCardDataType("float", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * An offset from UTC time, in hours and minutes (e.g. "-0500").
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
 	 */
 	public static final VCardDataType UTC_OFFSET = new VCardDataType("utc-offset", VCardVersion.V4_0);
 
 	/**
+	 * <p>
+	 * A standardized abbreviation for a language (e.g. "en-us" for American
+	 * English).
+	 * </p>
 	 * <b>Supported versions:</b> {@code 4.0}
+	 * @see <a href="http://tools.ietf.org/html/rfc5646">RFC 5646</a>
 	 */
 	public static final VCardDataType LANGUAGE_TAG = new VCardDataType("language-tag", VCardVersion.V4_0);
 
 	private final String name;
 	private final Set<VCardVersion> supportedVersions;
 
+	/**
+	 * @param name the data type name
+	 * @param supportedVersions the vCard versions it is supported in
+	 */
 	private VCardDataType(String name, VCardVersion... supportedVersions) {
 		this.name = name;
 		if (supportedVersions.length == 0) {
