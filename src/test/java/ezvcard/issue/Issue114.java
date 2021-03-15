48b763d473ca7faa40508d51bc34111c1c45d79d
Michael Angstadt <mike.angstadt@gmail.com>
2021-03-15 16:46:55 -0400

    Add unit test for issue 114
    https://github.com/mangstadt/ez-vcard/issues/114

---
+++ src/test/java/ezvcard/issue/Issue114.java
@@ -0,0 +1,36 @@
+package ezvcard.issue;
+
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.InputStream;
+
+/**
+ * @author Michael Angstadt
+ * @see "https://github.com/mangstadt/ez-vcard/issues/114"
+ */
+public class Issue114 {
+	@Test
+	public void test() throws Exception {
+		VCard vcard;
+		InputStream in = getClass().getResourceAsStream("issue114.vcf");
+		try {
+			vcard = Ezvcard.parse(in).first();
+		} finally {
+			in.close();
+		}
+
+		try {
+			Ezvcard.write(vcard).go();
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Property \"ADR\" has a parameter named \"LABEL\" whose value contains one or more invalid characters.  The following characters are not permitted: [ \\n \\r \" ]", e.getMessage());
+		}
+
+		Ezvcard.write(vcard).caretEncoding(true).go(); //should not throw any exceptions
+	}
+}
