8fa87623af50e465ab0b22da4e591404405f3b20
Alessandro Autiero <alautiero@gmail.com>
2023-02-10 20:06:53 +0100

    Added module-info.java

---
+++ src/main/moditect/module-info.java
@@ -0,0 +1,21 @@
+module ezvcard {
+  requires vinnie;
+  requires java.xml;
+  requires freemarker;
+  requires org.jsoup;
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.databind;
+  exports ezvcard;
+  exports ezvcard.io;
+  exports ezvcard.io.chain;
+  exports ezvcard.io.html;
+  exports ezvcard.io.json;
+  exports ezvcard.io.scribe;
+  exports ezvcard.io.text;
+  exports ezvcard.io.xml;
+  exports ezvcard.parameter;
+  exports ezvcard.property;
+  exports ezvcard.util;
+  exports ezvcard.util.org.apache.commons.codec;
+  exports ezvcard.util.org.apache.commons.codec.binary;
+}
