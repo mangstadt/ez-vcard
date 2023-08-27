d4c9ab9fb11f4ed6d83c32bdaa2089a73043bd9b
Mike Angstadt <mike.angstadt@gmail.com>
2023-08-27 18:07:58 -0400

    Update Ezvcard.VERSION for snapshot release

--- src/main/java/ezvcard/Ezvcard.java
+++ src/main/java/ezvcard/Ezvcard.java
@@ -99,7 +99,7 @@ public final class Ezvcard {
 	/**
 	 * The version of the library.
 	 */
-	public static final String VERSION = "0.12.1";
+	public static final String VERSION = "0.12.2-SNAPSHOT";
 
 	/**
 	 * The Maven group ID.
